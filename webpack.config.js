56d12f65be5fe0e1a98914f77a5a415a99cfbc6c
KelvinQiu <kelvinqiu802@outlook.com>
2024-08-15 21:29:54 +0800

    feat: use source map

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/main.tsx',
   mode: 'development',
-  devtool: false,
+  devtool: 'source-map',
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
   },
